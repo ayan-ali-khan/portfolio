59d62a4bc477e6af27e41e886bff8a8204b3aac9
Ayan Ali Khan <ayanalikhan154@gmail.com>
2025-07-30 19:12:38 +0530

    project initialised

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] bg-grid-white/[0.02] antialiased">
+      <div className="text-2xl text-center">Hello</div>
+    </main>
+  );
+}
