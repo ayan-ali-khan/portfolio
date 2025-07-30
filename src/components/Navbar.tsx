57543f4a2ac04ed5ef8754638e31666e2f22554e
Ayan Ali Khan <ayanalikhan154@gmail.com>
2025-07-30 23:55:43 +0530

    second commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,46 +1,30 @@
 "use client";
 import React, { useState } from "react";
-import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { FloatingNav } from "./ui/floating-navbar";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
 
 function Navbar({ className }: { className?: string }) {
     const [active, setActive] = useState<string | null>(null);
+
+    const navItems = [
+        {
+        name: "Home",
+        link: "/",
+        },
+        {
+        name: "Projects",
+        link: "/projects",
+        },
+        {
+        name: "Contact",
+        link: "/contact"
+        },
+    ];
+
     return (
         <div className={cn("fixed top-10 inset-x-0 max-w-xl mx-auto z-50", className)}>
-            <Menu setActive={setActive}>
-                <Link href={"/"}>
-                <MenuItem setActive={setActive} active={active} item="Home"></MenuItem>
-                </Link>
-                
-                <MenuItem setActive={setActive} active={active} item="Projects">
-                    <div className="  text-sm grid grid-cols-2 gap-10 p-4">
-                        <ProductItem
-                        title="Algochurn"
-                        href="https://algochurn.com"
-                        src="https://assets.aceternity.com/demos/algochurn.webp"
-                        description="Prepare for tech interviews like never before."
-                        />
-                        <ProductItem
-                        title="Tailwind Master Kit"
-                        href="https://tailwindmasterkit.com"
-                        src="https://assets.aceternity.com/demos/tailwindmasterkit.webp"
-                        description="Production ready Tailwind css components for your next project"
-                        />
-                        <ProductItem
-                        title="Moonbeam"
-                        href="https://gomoonbeam.com"
-                        src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.51.31%E2%80%AFPM.png"
-                        description="Never write from scratch again. Go from idea to blog in minutes."
-                        />
-                        
-                    </div>
-                </MenuItem>
-
-                <Link href={'/contact'}>
-                    <MenuItem setActive={setActive} active={active} item="Contact Us"></MenuItem>
-                </Link>
-            </Menu>
+            <FloatingNav navItems={navItems} />
         </div>
     )
 }
